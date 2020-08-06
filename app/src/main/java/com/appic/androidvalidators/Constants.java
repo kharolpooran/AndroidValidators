943776ff415596b7997b1316efd23714e60e31db
Pooran Kharol <pooran@dev.appicmobile.com>
2020-08-06 16:30:48 +0530

    added check on MainActivity

---
+++ app/src/main/java/com/appic/androidvalidators/Constants.java
@@ -0,0 +1,13 @@
+package com.appic.androidvalidators;
+
+public class Constants {
+
+    public static class PhoneNumber{
+        //Number
+        /*public static String SUCCESS = "Success";
+        public static String MAX_LENGTH = "MaxLength";
+        public static String IS_REQUIRED = "IsRequired";
+        public static String EMPTY = "Empty";*/
+    }
+
+}
--- app/src/main/java/com/appic/androidvalidators/MainActivity.java
+++ app/src/main/java/com/appic/androidvalidators/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.appic.androidvalidators.model.Email;
 import com.appic.androidvalidators.model.Password;
@@ -20,25 +21,31 @@ import com.appic.androidvalidators.model.Username;
 
 import java.util.HashMap;
 
+import static com.appic.androidvalidators.model.PhoneNumber.EMPTY;
+import static com.appic.androidvalidators.model.PhoneNumber.IS_REQUIRED;
+import static com.appic.androidvalidators.model.PhoneNumber.MAX_LENGTH;
+import static com.appic.androidvalidators.model.PhoneNumber.SUCCESS;
+
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner spinner;
     String[] country = {"Mr", "Mrs"};
-    EditText username,email,number,userpassword,confpassword;
+    EditText username, email, number, userpassword, confpassword;
     Button btn;
-
+    HashMap<String, Boolean> phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.spinner);
-        username=  findViewById(R.id.username);
-        email=  findViewById(R.id.email);
-        number=  findViewById(R.id.number);
-        userpassword=  findViewById(R.id.userpassword);
-        confpassword=  findViewById(R.id.confpassword);
-        btn=  findViewById(R.id.btn);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        number = findViewById(R.id.number);
+        userpassword = findViewById(R.id.userpassword);
+        confpassword = findViewById(R.id.confpassword);
+        btn = findViewById(R.id.btn);
         spinner.setOnItemSelectedListener(this);
         //Creating the ArrayAdapter instance having the country list
         ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, country);
@@ -55,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 .setCaseSensitive(true)
                 .build();
 
-        HashMap<String, Boolean> phoneNumber = new PhoneNumber.PhoneNumberBuilder("8866651281")
+        phoneNumber = new PhoneNumber.PhoneNumberBuilder("88666")
                 .setRequired(true)
                 .setMaxLenght(10)
                 .build();
@@ -64,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 .setRequired(true)
                 .build();
 
-        Password password = new Password.PasswordBuilder("123456")
+        final Password password = new Password.PasswordBuilder("123456")
                 .setMinValue(6)
                 .setMaxValue(16)
                 .setSpecialCharacterRequired(true)
@@ -74,7 +81,28 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 .build();
 
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (phoneNumber.get(IS_REQUIRED)) {
+                    if (phoneNumber.get(EMPTY)) {
+                        if (phoneNumber.get(MAX_LENGTH)) {
+                            if (phoneNumber.get(SUCCESS)) {
+                                Toast.makeText(MainActivity.this, "Correct", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, MAX_LENGTH, Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, EMPTY, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, IS_REQUIRED, Toast.LENGTH_SHORT).show();
+                }
 
+
+            }
+        });
     }
 
     @Override
